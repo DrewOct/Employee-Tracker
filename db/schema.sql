cd9fefe686450d69a64f1303bfbf7accf97884c4
Andrew Seymour <andrew.seymour1@gmail.com>
2022-09-06 17:57:14 -0500

    seeded database, starting routes

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  department_id INTEGER,
+  role_id INTEGER,
+  salary_id INTEGER,
+  manager_id INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    CONSTRAINT fk_salary FOREIGN KEY (salary_id) REFERENCES salaries(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES managers(id) ON DELETE SET NULL,
+);
+
+CREATE TABLE departments (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE salaries (
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  salary INTEGER
+);
+
+CREATE TABLE roles (
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  description TEXT);
---
+++ db/seeds.sql
@@ -0,0 +1,41 @@
+INSERT INTO departments (name, description)
+VALUES
+('Sales', '<description>'),
+('Research', '<description>'),
+('Finance', '<description>'),
+('Legal', '<description>');
+
+INSERT INTO employees (first_name, last_name, department_id, role_id)
+VALUES
+('Blando', 'JKalrissian', 2,3),
+('Ronald', 'Nortsaswagger', 1, 2),
+('Jucilla', 'Largess', 1, 2), 
+('Brent', 'Nezzar', 3, 2),
+('Johann', 'MacReady', 2, 1),
+('Jathan', 'Solello', 2, 1),
+('Jamir', 'Ashad', 2, 2),
+('Anna', 'Seelo', 1, 2),
+('Lakeisha', 'Baird', 1, 1),
+('Adam', 'Scott', 1, 2),
+('Ryan', 'Taylor', 3, 1),
+('Dustin', 'Manhoff', 3, 2),
+('William', 'Robins', 4, 1),
+('Baron', 'Karza', 2, 3),
+('Shanice', 'Burks', 4, 2),
+('Asha', 'Venkatasuban', 4, 2),
+('Lucas', 'Hoodle', 2, 2),
+('Mateo', 'Cruz', 2, 2),
+('Enrique', 'Grande', 2, 2),
+('Marnella', 'Suendo', 1, 2);
+
+INSERT INTO roles (name, description)
+VALUES
+('Sales Lead', '<description>'),
+('Project Lead', '<description>'),
+('Account Manager', '<description>'),
+('Legal Team Lead', '<description>'),
+('Sales Associate', '<description>'),
+('Science Staff', '<description>'),
+('Medical Science Liaison', '<description>'),
+('Accountant', '<description>'),
+('Laweyer', '<description>');
